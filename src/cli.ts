fe2eb4d383e92f496263445837b5a169cbaf24d5
thomas <thomas.chang7@gmail.com>
2025-01-15 22:15:33 -0800

    setup file selector

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { greet } from './index';
+import { greet } from './index.js';
+import inquirer from 'inquirer';
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
+import fs from 'fs';
+
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
 
 const program = new Command();
 
@@ -10,9 +15,22 @@ program
   .version('1.0.0');
 
 program
-  .argument('[name]', 'name to greet', 'World')
-  .action((name: string) => {
-    console.log(greet(name));
+  .argument('[path]', 'Path to start file selection from')
+  .action(async (path: string, options) => {
+    const answer = await inquirer.prompt([
+        {
+          type: 'file-tree-selection',
+          name: 'files',
+          message: 'Select files to convert:',
+          multiple: true,
+          root: path,
+        //   onlyShowValid: true,
+        //   validate: (item: string) => {
+        //     return item.endsWith('.json') || fs.lstatSync(item).isDirectory();
+        //   }
+        }
+      ]);
+      console.log('Selected files:', answer.files);
   });
 
 program.parse();
